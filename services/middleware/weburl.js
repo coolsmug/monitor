74ec8dce9d622944b43b683b7a9314b227c9fce4
Yusuph Idris Olatunji <88779588+coolsmug@users.noreply.github.com>
2025-09-12 01:30:51 +0100

    dd

--- services/middleware/weburl.js
+++ services/middleware/weburl.js
@@ -2,8 +2,8 @@ const School = require("../models/school.name");
 
 module.exports = async function subdomainRouter(req, res, next) {
   const host = req.headers.host;
-  // const mainDomain = "monitrex.work";
-    const mainDomain = "localhost:2022";
+  const mainDomain = "monitrex.work";
+    // const mainDomain = "localhost:2022";
 
   if (!host.endsWith(mainDomain)) {
     return next();
